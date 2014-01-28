97892ac6ef207f5d0a627f9cbfb75ac164b680e6
Teun Duynstee <github@duynstee.com>
2014-01-28 19:23:44 +0100

    introduced mongoose introduced passport added User model login, signup, logout screens

--- app.js
+++ app.js
@@ -3,15 +3,26 @@
  * Module dependencies.
  */
 
+// Bootstrap models
+var fs = require('fs');
+var models_path = __dirname + '/models'
+fs.readdirSync(models_path).forEach(function (file) {
+  require(models_path + '/' + file);
+});
+
 var express = require('express');
 var routes = require('./config/routes');
 var config = require('./config')
 var http = require('http');
 var path = require('path');
-
+var passport = require('passport');
+var mongoose = require('mongoose');
+mongoose.connect(config.data.userStore.conn);
 var app = express();
+var auth = require('./config/middlewares/authorization');
 
-config.appCfg(app, config);
+config.passportCfg(passport, config);
+config.appCfg(app, config, passport);
 routes.init(app);
 
 http.createServer(app).listen(app.get('port'), function(){
--- public/js/tphq.js
+++ public/js/tphq.js
@@ -238,7 +238,7 @@ var TPHQ = (function()
         alert("error: " + g);
       });
     $('.share-facebook').click(function () {
-      var shareUrl = 'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=' + document.location.href + '&p[images][0]=' + scope.model.plan.images()[0].large() + '&p[title]=' + document.title + '&p[summary]=Check out my travel plan';
+      var shareUrl = 'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=' + document.location.href + '&p[images][0]=' + scope.model.plan.images()[0].large() + '&p[title]=' + encodeURIComponent(document.title) + '&p[summary]=Check out my travel plan';
       window.open(shareUrl, '_blank');
       return false;
     });
