5d4a125fd7194d05042891812bc8d108ff135538
Teun Duynstee <github@duynstee.com>
2014-05-17 16:15:23 +0200

    Added page + handler for /my When saving, the username is included in the author block

--- data/plan.js
+++ data/plan.js
@@ -1,4 +1,4 @@
-﻿var async = require('async');
+var async = require('async');
 var cfg = require('../config');
 var fs = require('fs');
 var folder = cfg.data.planPath || __dirname + '/plans/';
@@ -53,11 +53,11 @@ exports.getPlan = function (id, success) {
 }
 exports.getAccessFor = function (plan, req) {
   // TODO: check right for notes
-  if(req.session.passport && exports.canSave(plan.id, req.user)) return "full";
+  if(req.session.passport && exports.canSave(plan, req.user)) return "full";
   return "none";
 }
-exports.canSave = function (planID, user) {
-  return(user && user.username == 'teun');
+exports.canSave = function (plan, user) {
+  return(user && (user.username == plan.author.id || !plan.author.id));
 }
 
 exports.savePlan = function (id, plan, options) {
@@ -67,8 +67,9 @@ exports.savePlan = function (id, plan, options) {
     plan.id = id;
     plan.server = oldPlan.server;
     if(opt.owner)plan.owner = opt.owner;
-    if (opt.author) {
+    if(opt.author) {
       plan.author.name = opt.author;
+      plan.author.id = opt.userid;
     }
     all[id] = plan;
     var outputFilename = folder + id + ".json";
@@ -97,11 +98,23 @@ exports.urlFor = function (plan, append) {
 }
 exports.getSome = function (success) {
   var res = [];
-  exports.getPlan('dw2wtz', function (p) {
-    res.push(p);
-    exports.getPlan('wy2ore', function (p) {
-      res.push(p);
-      success(res);
-    });
+  whenPlansLoaded(function (all) {
+    for(var key in all){
+      res.push(all[key]);
+    }
+    success(res);
+  });
+}
+exports.getMine = function (user, success) {
+  var res = [];
+  whenPlansLoaded(function (all) {
+    for(var key in all){
+      var plan = all[key];
+      if(plan.author.id == user.username){
+        res.push(plan);
+      }
+    }
+		console.log(res);
+    success(res);
   });
 }
